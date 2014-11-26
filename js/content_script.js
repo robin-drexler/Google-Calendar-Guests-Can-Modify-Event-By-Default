2038c0ee7fc5706f4ac3bfe0b3136f303927bc8d
Robin Drexler <drexler.robin@gmail.com>
2014-11-26 18:58:27 +0100

    implement mvp

--- js/content_script.js
+++ js/content_script.js
@@ -1,3 +1,19 @@
-$(function() {
-    alert('Yay, I\'m an extension :)');
-});
+(function() {
+  function onLoad() {
+    // checkbox id is something like :31guests-modify
+    // the number does actually change
+    // it's not even a valid selector ¯\_(ツ)_/¯
+    var modifyEventCheckbox = document.querySelector('input[id$=guests-modify]');
+
+    if (modifyEventCheckbox) {
+      modifyEventCheckbox.checked = true;
+    }
+  }
+
+  window.addEventListener('hashchange', function() {
+    // delay execution, because DOM might not be ready immediately
+    // after event was fired
+    window.setTimeout(onLoad, 250);
+  });
+  onLoad();
+})();
