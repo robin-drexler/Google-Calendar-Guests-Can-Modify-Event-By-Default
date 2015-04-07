7a79786fd6a8196aca0a06608e8bf5726ba91caf
Robin Drexler <drexler.robin@gmail.com>
2015-04-07 08:31:48 +0200

    activate the checkbox by mimicing click

--- js/content_script.js
+++ js/content_script.js
@@ -1,12 +1,20 @@
 (function() {
+
   function onLoad() {
     // checkbox id is something like :31guests-modify
     // the number does actually change
     // it's not even a valid selector ¯\_(ツ)_/¯
     var modifyEventCheckbox = document.querySelector('input[id$=guests-modify]');
+    var clickEvent = new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+      view: window
+    });
 
-    if (modifyEventCheckbox) {
-      modifyEventCheckbox.checked = true;
+    if (modifyEventCheckbox && !modifyEventCheckbox.checked) {
+      // activate the checkbox by mimicing click
+      // needed so the checkbox stays checked, after switching tabs to e.g. to "find a time"
+      modifyEventCheckbox.dispatchEvent(clickEvent)
     }
   }
 
